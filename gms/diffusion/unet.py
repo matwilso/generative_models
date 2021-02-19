0b28ef2c72c09e8ddb4b979ddcdef811d99a117c
Matthew Wilson <mattwilsonmbw@gmail.com>
2021-02-19 16:00:02 -0700

    lay some more stuff out. but still need to clean up more and make it run

--- gms/diffusion/unet.py
+++ gms/diffusion/unet.py
@@ -40,9 +40,9 @@ class ResnetBlock(nn.Module):
     x += nn.Linear(C, out_ch)(swish(temb))[:, None, None, :]
 
     # add in timestep embedding
-    h += nn.dense(nonlinearity(temb), name='temb_proj', num_units=out_ch)[:, None, None, :]
+    h += nn.dense(swish(temb), name='temb_proj', num_units=out_ch)[:, None, None, :]
 
-    h = nonlinearity(normalize(h, temb=temb, name='norm2'))
+    h = swish(normalize(h, temb=temb, name='norm2'))
     h = tf.nn.dropout(h, rate=dropout)
     h = nn.conv2d(h, name='conv2', num_units=out_ch, init_scale=0.)
 
@@ -56,84 +56,90 @@ class ResnetBlock(nn.Module):
     print('{}: x={} temb={}'.format(tf.get_default_graph().get_name_scope(), x.shape, temb.shape))
     return x + h
 
+class SelfAttention(nn.Module):
+  def __init__(self, block_size, C):
+    super().__init__()
+    self.block_size = block_size
+    assert C.n_embed % C.n_head == 0
+    # key, query, value projections for all heads
+    self.key = nn.Linear(C.n_embed, C.n_embed)
+    self.query = nn.Linear(C.n_embed, C.n_embed)
+    self.value = nn.Linear(C.n_embed, C.n_embed)
+    # output projection
+    self.proj = nn.Linear(C.n_embed, C.n_embed)
+    # causal mask to ensure that attention is only applied to the left in the input sequence
+    self.C = C
 
-def attn_block(x, *, name, temb):
-  B, H, W, C = x.shape
-  with tf.variable_scope(name):
-    h = normalize(x, temb=temb, name='norm')
-    q = nn.nin(h, name='q', num_units=C)
-    k = nn.nin(h, name='k', num_units=C)
-    v = nn.nin(h, name='v', num_units=C)
+  def forward(self, x, layer_past=None):
+    B, T, C = x.size()
+    # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+    k = self.key(x).view(B, T, self.C.n_head, C // self.C.n_head).transpose(1, 2)  # (B, nh, T, hs)
+    q = self.query(x).view(B, T, self.C.n_head, C // self.C.n_head).transpose(1, 2)  # (B, nh, T, hs)
+    v = self.value(x).view(B, T, self.C.n_head, C // self.C.n_head).transpose(1, 2)  # (B, nh, T, hs)
+    # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+    att = (q @ k.transpose(-2, -1)) * (1.0 / np.sqrt(k.size(-1)))
+    att = F.softmax(att, dim=-1)
+    y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+    y = y.transpose(1, 2).contiguous().view(B, T, C)  # re-assemble all head outputs side by side
+    # output projection
+    y = self.proj(y)
+    return y
 
-    w = tf.einsum('bhwc,bHWc->bhwHW', q, k) * (int(C) ** (-0.5))
-    w = tf.reshape(w, [B, H, W, H * W])
-    w = tf.nn.softmax(w, -1)
-    w = tf.reshape(w, [B, H, W, H, W])
+class HoUnet(nn.Module):
+  def __init__(self, C):
+    super().__init__()
+    # model(x, *, t, y, name, num_classes, reuse=tf.AUTO_REUSE, ch, out_ch, ch_mult=(1, 2, 4, 8), num_res_blocks, attn_resolutions, dropout=0., resamp_with_conv=True):
+    self._middle = nn.Sequential(
+      ResnetBlock(),
+      SelfAttention(),
+      ResnetBlock(),
+    )
 
-    h = tf.einsum('bhwHW,bHWc->bhwc', w, v)
-    h = nn.nin(h, name='proj_out', num_units=C, init_scale=0.)
+  def forward(x):
+    B, S, _, _ = x.shape
+    num_resolutions = len(ch_mult)
 
-    assert h.shape == x.shape
-    print(tf.get_default_graph().get_name_scope(), x.shape)
-    return x + h
+    assert num_classes == 1 and y is None, 'not supported'
+    del y
 
-
-def model(x, *, t, y, name, num_classes, reuse=tf.AUTO_REUSE, ch, out_ch, ch_mult=(1, 2, 4, 8), num_res_blocks,
-          attn_resolutions, dropout=0., resamp_with_conv=True):
-  B, S, _, _ = x.shape
-  assert x.dtype == tf.float32 and x.shape[2] == S
-  assert t.dtype in [tf.int32, tf.int64]
-  num_resolutions = len(ch_mult)
-
-  assert num_classes == 1 and y is None, 'not supported'
-  del y
-
-  with tf.variable_scope(name, reuse=reuse):
     # Timestep embedding
-    with tf.variable_scope('temb'):
-      temb = nn.get_timestep_embedding(t, ch)
-      temb = nn.dense(temb, name='dense0', num_units=ch * 4)
-      temb = nn.dense(nonlinearity(temb), name='dense1', num_units=ch * 4)
-      assert temb.shape == [B, ch * 4]
+    temb = nn.get_timestep_embedding(t, ch)
+    temb = nn.dense(temb, name='dense0', num_units=ch * 4)
+    temb = nn.dense(swish(temb), name='dense1', num_units=ch * 4)
+    assert temb.shape == [B, ch * 4]
 
     # Downsampling
     hs = [nn.conv2d(x, name='conv_in', num_units=ch)]
     for i_level in range(num_resolutions):
-      with tf.variable_scope('down_{}'.format(i_level)):
-        # Residual blocks for this resolution
-        for i_block in range(num_res_blocks):
-          h = resnet_block(
-              hs[-1], name='block_{}'.format(i_block), temb=temb, out_ch=ch * ch_mult[i_level], dropout=dropout)
-          if h.shape[1] in attn_resolutions:
-            h = attn_block(h, name='attn_{}'.format(i_block), temb=temb)
-          hs.append(h)
-        # Downsample
-        if i_level != num_resolutions - 1:
-          hs.append(downsample(hs[-1], name='downsample', with_conv=resamp_with_conv))
+      # Residual blocks for this resolution
+      for i_block in range(num_res_blocks):
+        h = resnet_block(
+            hs[-1], name='block_{}'.format(i_block), temb=temb, out_ch=ch * ch_mult[i_level], dropout=dropout)
+        if h.shape[1] in attn_resolutions:
+          h = attn_block(h, name='attn_{}'.format(i_block), temb=temb)
+        hs.append(h)
+      # Downsample
+      if i_level != num_resolutions - 1:
+        hs.append(downsample(hs[-1], name='downsample', with_conv=resamp_with_conv))
 
     # Middle
-    with tf.variable_scope('mid'):
-      h = hs[-1]
-      h = resnet_block(h, temb=temb, name='block_1', dropout=dropout)
-      h = attn_block(h, name='attn_1'.format(i_block), temb=temb)
-      h = resnet_block(h, temb=temb, name='block_2', dropout=dropout)
+    h = self._middle(hs[-1])
 
     # Upsampling
     for i_level in reversed(range(num_resolutions)):
-      with tf.variable_scope('up_{}'.format(i_level)):
-        # Residual blocks for this resolution
-        for i_block in range(num_res_blocks + 1):
-          h = resnet_block(tf.concat([h, hs.pop()], axis=-1), name='block_{}'.format(i_block),
-                           temb=temb, out_ch=ch * ch_mult[i_level], dropout=dropout)
-          if h.shape[1] in attn_resolutions:
-            h = attn_block(h, name='attn_{}'.format(i_block), temb=temb)
-        # Upsample
-        if i_level != 0:
-          h = upsample(h, name='upsample', with_conv=resamp_with_conv)
+      # Residual blocks for this resolution
+      for i_block in range(num_res_blocks + 1):
+        h = resnet_block(tf.concat([h, hs.pop()], axis=-1), name='block_{}'.format(i_block),
+                         temb=temb, out_ch=ch * ch_mult[i_level], dropout=dropout)
+        if h.shape[1] in attn_resolutions:
+          h = attn_block(h, name='attn_{}'.format(i_block), temb=temb)
+      # Upsample
+      if i_level != 0:
+        h = upsample(h, name='upsample', with_conv=resamp_with_conv)
     assert not hs
 
     # End
-    h = nonlinearity(normalize(h, temb=temb, name='norm_out'))
+    h = swish(normalize(h, temb=temb, name='norm_out'))
     h = nn.conv2d(h, name='conv_out', num_units=out_ch, init_scale=0.)
     assert h.shape == x.shape[:3] + [out_ch]
     return h
