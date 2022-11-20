ee1d595aaeb9723757fefb7eeec89714f4afb009
Matthew Wilson <mattwilsonmbw@gmail.com>
2022-11-20 13:43:35 -0800

    Remove legacy diffusion model

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,8 +9,6 @@ models = sorted(common.discover_models().keys())
 
 @pytest.mark.parametrize("model_name", models)
 def test_all_models(model_name):
-    if "legacy" in model_name:
-        return
     cmd = f'python -m gms.main --model={model_name} --epochs=1 --logdir logs/pytest/{model_name}'
     print(cmd)
     subprocess.run(cmd, shell=True, check=True)
